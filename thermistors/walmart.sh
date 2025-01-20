147a5714c8128d2e57bb6e766f86b7c11b73e47d
Justin Roeder <92489099+delphijustin@users.noreply.github.com>
2025-01-19 18:16:27 -0600

    Update walmart.sh

--- thermistors/walmart.sh
+++ thermistors/walmart.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #The link to buy this thermistor is in the csv file.
-./thermistor-linux /unit=f /ino=../src/MeterOS/MeterOS.ino
+./thermistor-linux /unit=f /ino=../src/MeterOS/MeterOS.ino /csv=precalibrated/walmart.csv
